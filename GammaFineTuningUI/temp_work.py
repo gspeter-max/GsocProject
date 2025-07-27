5a68912d7fbcde179b289b2a57598a685b3ac670
gspeter-max <gspeter67@gmail.com>
2025-07-27 12:37:12 +0530

    torch --> tensorflow conversion is added and work fine

--- GammaFineTuningUI/temp_work.py
+++ GammaFineTuningUI/temp_work.py
@@ -5,27 +5,23 @@ import onnx-tf
 
 
 class ConvertModel:
-    def __init__( self , TorchToTenserFlow : bool = False , TensorFlowToTorch : bool = False ): 
+    def __init__( self , TorchToTenserFlow : bool = False , TensorFlowToTorch : bool = False, \
+            WhereStored : str = './tfConvetedModel/'): 
         self.TorchToTenserFlow = TorchToTenserFlow 
         self.TensorFlowToTorch = TensorFlowToTorch
-
+        self.WhereStored = WhereStored
 
     def __call__( self, Model , Input):
         torch.onnx.export(     
-                    Model , 
-                    ( Input,), 
-                    dynamo = True , 
-                    f = './output',
-                    verbose = True,
-                    input_names = ['doc'],
-                    output_names = ['claim']
+                Model , 
+                ( Input,), 
+                dynamo = True , 
+                f = './output',
+                verbose = True,
+                input_names = ['doc'],
+                output_names = ['claim']
             )
-        onnxModel = onnx.load('./output')
-        PreparedModel = onnx.backend.prepare(onnxModel) 
-        PrepareModel.export(                                                            
-
-
-         
-
-
+        onnxModel = onnx.load('./model_stored')
+        PreparedModel = onnx_tf.backend.prepare(onnxModel) 
+        PrepareModel.export_graph( self.WhereStored ) 
 
