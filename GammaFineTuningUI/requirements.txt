4740ae53dc523b8c339d75640d23096345755d87
gspeter-max <gspeter67@gmail.com>
2025-07-27 11:21:57 +0530

    requirements.txt is added for perfect onnx_tf import

---
+++ GammaFineTuningUI/requirements.txt
@@ -0,0 +1,6 @@
+tensorflow==2.15.0
+keras==2.15.0
+numpy==1.26.4
+tensorflow-probability==0.23.0
+onnx==1.18.0
+onnx-tf==1.10.0
--- GammaFineTuningUI/temp_work.py
+++ GammaFineTuningUI/temp_work.py
@@ -1,37 +1,31 @@
-from transformers import AutoTokenizer
-import torch
-import copy
+
+import torch 
+import onnx 
+import onnx-tf 
 
 
-tokenizer = AutoTokenizer.from_pretrained('google/gemma-3-1b-pt')
-tokenizer.pad_token = tokenizer.eos_token
-tokenizer.add_special_tokens({'additional_special_tokens' : ['[SEP]']})
+class ConvertModel:
+    def __init__( self , TorchToTenserFlow : bool = False , TensorFlowToTorch : bool = False ): 
+        self.TorchToTenserFlow = TorchToTenserFlow 
+        self.TensorFlowToTorch = TensorFlowToTorch
 
-def map_function(example):
+
+    def __call__( self, Model , Input):
+        torch.onnx.export(     
+                    Model , 
+                    ( Input,), 
+                    dynamo = True , 
+                    f = './output',
+                    verbose = True,
+                    input_names = ['doc'],
+                    output_names = ['claim']
+            )
+        onnxModel = onnx.load('./output')
+        PreparedModel = onnx.backend.prepare(onnxModel) 
+        PrepareModel.export(                                                            
 
 
-    text = [f'{doc} [SEP] {claim}' for doc , claim in zip(example['doc'], example['claim'])]
-    tokenized = tokenizer( text, padding = 'max_length' ,return_tensors = 'pt', max_length = 128, truncation = True )
+         
 
-    labels = tokenized.input_ids.clone()
-    SEPTokenId = tokenizer.convert_tokens_to_ids('[SEP]')
 
-    bool_mask = labels == SEPTokenId
-    mask_index = torch.argwhere(bool_mask)
-
-    rows, columns  = zip(mask_index[0], mask_index[1])
-
-    for r, c  in zip(rows, columns):
-        labels[r,:c] = -100
-    
-    return {
-            'input_ids' : tokenized.input_ids,
-            'attention_mask' : tokenized.attention_mask,
-            'labels' : labels
-            }
-
-tokenized_data = dataset.map(map_function, batched = True, remove_columns = dataset.column_names)
-tokenized_data.set_format( 
-                          device = 'cuda'
-                          ) 
 
