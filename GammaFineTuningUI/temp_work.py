093550358fd0fec8df23ef7885389a1a08ac7ed0
gspeter-max <gspeter67@gmail.com>
2025-07-27 12:39:29 +0530

    torch --> tensorflow conversion is added and work fine

--- GammaFineTuningUI/temp_work.py
+++ GammaFineTuningUI/temp_work.py
@@ -24,4 +24,25 @@ class ConvertModel:
         onnxModel = onnx.load('./model_stored')
         PreparedModel = onnx_tf.backend.prepare(onnxModel) 
         PrepareModel.export_graph( self.WhereStored ) 
+        ''' 
+        import tensorflow as tf
+
+        #Load the model from thedirectory
+        loaded_tf_model = tf.saved_model.load('/content/main')
+
+        # You can now use the loaded model to make predictions
+        # For example, if you have some input data:
+        # input_data = ...
+        # predictions = loaded_tf_model(input_data)
+        import numpy as np
+
+        # Create some dummy input data with the correct shape
+        input_data = np.random.rand(1, 10).astype(np.float32)
+
+        # Get the output from the model
+        predictions = loaded_tf_model(x=input_data)
+
+        # Print the predictions
+        print(predictions)
+        ''' 
 
