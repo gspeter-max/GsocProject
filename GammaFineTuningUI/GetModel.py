0ef7f6be13396bf75ee8eb8a94b9dc3f054b5cd9
gspeter-max <gspeter67@gmail.com>
2025-07-28 09:35:50 +0530

    thing are fine right now

--- GammaFineTuningUI/GetModel.py
+++ GammaFineTuningUI/GetModel.py
@@ -1,53 +1,37 @@
 
+from transformers import TFAutoModelForCausalLM 
 import torch 
 import onnx 
 import onnx-tf 
+import shutil
 
 class ConvertModel:
     def __init__( self , GetInTensorFlow : bool = False ,GetInTorch : bool = False, \
-            WhereStored : str = './tfConvetedModel/'): 
+            WhereStored : str = './tfConvetedModel/',\
+            GetInGGUF : bool = False
+                 ): 
         self.GetInTensorflow = GetInTensorflow
         self.GetInTorch = GetInTorch
         self.WhereStored = WhereStored
+        self.GetInGGUF = GetInGGUF
 
-    def __call__( self, Model , Input):
-        if self.GetInTensorflow :
-            torch.onnx.export(     
-                    Model , 
-                    (Input,), 
-                    dynamo = True , 
-                    f = './output',
-                    verbose = True,
-                    input_names = ['doc'],
-                    output_names = ['claim']
-                )
-            onnxModel = onnx.load('./model_stored')
-            PreparedModel = onnx_tf.backend.prepare(onnxModel) 
-            PrepareModel.export_graph( self.WhereStored ) 
-        ''' 
-        import tensorflow as tf
-
-        #Load the model from thedirectory
-        loaded_tf_model = tf.saved_model.load('/content/main')
-
-        # You can now use the loaded model to make predictions
-        # For example, if you have some input data:
-        # input_data = ...
-        # predictions = loaded_tf_model(input_data)
-        import numpy as np
-
-        # Create some dummy input data with the correct shape
-        input_data = np.random.rand(1, 10).astype(np.float32)
-
-        # Get the output from the model
-        predictions = loaded_tf_model(x=input_data)
-
-        # Print the predictions
-        print(predictions)
-        ''' 
+    def __call__( self, Model,Tokenizer , Input):
         
-        if self.GetInTorch: 
-            torch.save( model.state_dict() , self.WhereStored ) 
+        if self.GetInTensorFlow: 
+            ptSaveFile = './TempStore', 
+            Model.save_pretrained(ptSaveFile) 
+            Tokenizer.save_pretrained(ptSaveFile)
+    
+            model = TFAutoModelForCausalLM.from_pretrained( ptSaveFile, from_pt = True ) 
+
+            model.save_pretrained( self.WhereStored )
+            Tokenizer.save_pretrained( self.WhereStored )
+            shutil.rmtree(ptSaveFile)
+
+        elif self.GetInTorch: 
+            Model.save_pretrained( self.WhereStored )
+
+
 
         return f'model is stored in {self.WhereStored} , {self.GetInTorch} format'
 
