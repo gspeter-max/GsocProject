29142e1f85713d81a372919d46f27c9ccdcb5216
gspeter-max <gspeter67@gmail.com>
2025-07-29 11:00:56 +0530

    ' complte'

--- GammaFineTuningUI/backend/ModelingAndTuning.py
+++ GammaFineTuningUI/backend/ModelingAndTuning.py
@@ -43,24 +43,24 @@ tokenizer.add_special_tokens({'additional_special_tokens' : ["[SEP]"]})
 def map_function(example):
 
     text = [f'{doc} [SEP] {claim} ' for doc , claim in zip(example['doc'], example['claim'])]
-        tokenized = tokenizer(
-                text,
-                padding = HyperparameterConfig.get('TokenizationConfig')['padding'] ,
-                return_tensors = 'pt',
-                max_length = HyperparameterConfig.get('TokenizationConfig')['max_length'],
-                truncation = HyperparameterConfig.get('TokenizationConfig')['truncation']
-        )
-        labels = tokenized.input_ids.clone()
-        SEPTokenId = tokenizer.convert_tokens_to_ids("[SEP]")
-        mask_index = torch.argwhere(labels == SEPTokenId )
-        rows, columns  = zip(mask_index[0], mask_index[1])
-        for r, c  in zip(rows, columns):
-                labels[r,:c] = -100
-        return {
-            'input_ids' : tokenized.input_ids,
-            'attention_mask' : tokenized.attention_mask,
-            'labels' : labels
-            }
+    tokenized = tokenizer(
+            text,
+            padding = HyperparameterConfig.get('TokenizationConfig')['padding'] ,
+            return_tensors = 'pt',
+            max_length = HyperparameterConfig.get('TokenizationConfig')['max_length'],
+            truncation = HyperparameterConfig.get('TokenizationConfig')['truncation']
+    )
+    labels = tokenized.input_ids.clone()
+    SEPTokenId = tokenizer.convert_tokens_to_ids("[SEP]")
+    mask_index = torch.argwhere(labels == SEPTokenId )
+    rows, columns  = zip(mask_index[0], mask_index[1])
+    for r, c  in zip(rows, columns):
+            labels[r,:c] = -100
+    return {
+        'input_ids' : tokenized.input_ids,
+        'attention_mask' : tokenized.attention_mask,
+        'labels' : labels
+        }
 
 tokenized_data = dataset.map(map_function, batched = True, remove_columns = dataset.column_names)
 class ModelLoadingAndTuning:
