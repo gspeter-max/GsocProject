3e1d3fb80532823aa0f8c4f66bc5234e85d40650
gspeter-max <gspeter67@gmail.com>
2025-08-06 17:03:03 +0530

    class generate_response is done and generate well

---
+++ notebook-x/__init__.py
@@ -0,0 +1,6 @@
+import os 
+
+os.environ['LANGCHAIN_TRACING_V2'] = 'true'
+os.environ['LANGCHAIN_API_KEY'] = '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'
+os.environ['LANGCHAIN_PROJECT'] = 'MyFirstLangSmithProject'
+
--- notebook-x/generate_content.py
+++ notebook-x/generate_content.py
@@ -1,63 +1,28 @@
-from data import url_data 
-from langchain_text_splitters.character import RecursiveCharacterTextSplitter 
+from data import url_data
+from langchain_text_splitters.character import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_core.vectorstores import InMemoryVectorStore
 from langchain.chat_models import init_chat_model
-from langchain.prompts import ChatPromptTemplate  
-from langchain.memory import ConversationSummaryMemory
+from langchain.prompts import ChatPromptTemplate
+from langchain.memory import ConversationSummaryBufferMemory
+import os
 
 
 class generate_reponse:
-    def __init__( self, model_name = 'gemini-2.5-flash' ):
-        self.model_name = model_name
+    def __init__( self, model_name = 'gemini-2.5-flash',first_query = 'what is ai'):
 
-    def get_similar_content( self, states : dict ):
-        similar_content = self.vectorstore.similarity_search(query = states['question'] )
-        states['context'] = similar_content
-        return states 
+        self.llm = init_chat_model(model_name, model_provider="google-genai", \
+            google_api_key = 'AIzaSyDKUGAMTjpKpNxmVGU7Wi3pMM1QTumsYNI'
+            )
 
-    def generate_content( self,states : dict ):
-        context = '\n\n'.join( document.page_content for document in states['context'] )
-        raw_prompt = {
-                'chat_history' : self.memory.load_memory_variables({})['chat_history'],
-                'context': context,
-                'question': states['question']
-                }
-
-        self.prompt = self.prompt.invoke(raw_prompt)
-        result = self.llm.invoke(self.prompt)
-        self.memory.chat_memory.add_user_message(states['question'])
-        self.memory.chat_memory.add_ai_message(result)
-
-        return {'Answer' : result}
-
-    def create_response( self,query):
-
-        url_data_class = url_data(query = query )
-        url_docs = url_data_class.get_all_data() 
-
-        rc_text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size = 500,
-            chunk_overlap = 20
-        )
-
-        splited_docs = rc_text_splitter.split_documents( url_docs )
-
-        self.llm = init_chat_model( model = self.model_name , model_provider = 'google_genai' ,\
+        self.embedding = GoogleGenerativeAIEmbeddings( model = 'models/gemini-embedding-001', \
                 google_api_key = 'AIzaSyDKUGAMTjpKpNxmVGU7Wi3pMM1QTumsYNI'
                 )
 
-        embedding = GoogleGenerativeAIEmbeddings( model = 'models/gemini-embedding-001', \
-                google_api_key = 'AIzaSyDKUGAMTjpKpNxmVGU7Wi3pMM1QTumsYNI'
-                )
-
-        self.vectorstore = InMemoryVectorStore( embedding )
-        self.vectorstore.add_documents(splited_docs)
-        self.memory = ConversationSummaryMemory(
+        self.memory = ConversationSummaryBufferMemory(
                 memory_key = 'chat_history',
-                max_num_tokens = 200,
-                llm = llm,
-                return_messages = True 
+                return_messages = True,
+                llm  = self.llm
                 )
 
         self.prompt = ChatPromptTemplate.from_template(
@@ -65,20 +30,57 @@ class generate_reponse:
                 Previous_Conversation : {chat_history}
                 Context : {context}
                 Question : {question}
-                Answer : 
+                Answer :
             '''
-            ) 
-    
+            )
+
+    def get_similar_content( self, states : dict ):
+        similar_content = self.vectorstore.similarity_search(query = states['question'] )
+        states['context'] = similar_content
+        return states
+
+    def generate_content( self,states : dict ):
+        context = '\n\n'.join( document.page_content for document in states['context'] )
+        print(f"chat history : {self.memory.load_memory_variables({})['chat_history']}")
+        raw_prompt = {
+                'chat_history' : self.memory.load_memory_variables({})['chat_history'],
+                'context': context,
+                'question': states['question']
+                }
+
+        llm_prompt = self.prompt.invoke(raw_prompt).to_string()
+        result = self.llm.invoke(llm_prompt)
+        self.memory.chat_memory.add_user_message(states['question'])
+        self.memory.chat_memory.add_ai_message(result)
+
+        return {'Answer' : result}
+
+    def create_response( self,url_docs):
+
+        rc_text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size = 1000,
+            chunk_overlap = 20
+        )
+
+        splited_docs = rc_text_splitter.split_documents( url_docs )
+
+        self.vectorstore = InMemoryVectorStore( self.embedding )
+        self.vectorstore.add_documents(splited_docs)
+
     def get_response(self,query):
-        
-        self.create_response(query)
+
+        url_data_class = url_data(query = query )
+        url_docs = url_data_class.get_all_data()
+
+        self.create_response( url_docs )
         states = self.get_similar_content( states = {'question' : query} )
         generated_content = self.generate_content( states )
+        answer = generated_content['Answer'].content
 
-        return {'Answer' : generated_content["Answer"].content}
+        def remove_bad_things( text ):
+            return re.sub( r" ```.*?\n|\n```", "", text, flags = re.DOTALL )
 
+        return {'Answer' : remove_bad_things(answer) }
 
 
 generate = generate_reponse()
-response = generate.get_response( 'what is llm ? ')
-
