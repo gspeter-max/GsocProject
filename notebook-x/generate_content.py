c8f8e9c38ea2407764852790904c8ef33d64a868
gspeter-max <gspeter67@gmail.com>
2025-08-06 11:46:06 +0530

    something is added

--- notebook-x/generate_content.py
+++ notebook-x/generate_content.py
@@ -3,49 +3,75 @@ from langchain_text_splitters.character import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_core.vectorstores import InMemoryVectorStore
 from langchain.chat_models import init_chat_model
-from langchain import hub
+from langchain.prompts import ChatPromptTemplate  
+from langchain.memory import ConversationSummaryMemory
 
 
-url_data_class = url_data(query = 'what is llm')
-url_docs = url_data_class.get_all_data() 
+class generate_reponse:
+    def __init__( self, model_name = 'gemini-2.5-flash' ):
+        self.model_name = model_name
 
-rc_text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size = 500,
-    chunk_overlap = 20
-)
+    def create_response( self,query):
 
-splited_docs = rc_text_splitter.split_documents( url_docs )
+        url_data_class = url_data(query = query )
+        url_docs = url_data_class.get_all_data() 
 
-llm = init_chat_model( model = 'gemini-2.5-flash', model_provider = 'google_genai' ,\
-        google_api_key = 'AIzaSyDKUGAMTjpKpNxmVGU7Wi3pMM1QTumsYNI'
+        rc_text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size = 500,
+            chunk_overlap = 20
         )
 
-embedding = GoogleGenerativeAIEmbeddings( model = 'models/gemini-embedding-001', \
-        google_api_key = 'AIzaSyDKUGAMTjpKpNxmVGU7Wi3pMM1QTumsYNI'
-        )
+        splited_docs = rc_text_splitter.split_documents( url_docs )
 
-vectorstore = InMemoryVectorStore( embedding )
-vectorstore.add_documents(splited_docs)
-prompt = hub.pull('rlm/rag-prompt')
+        llm = init_chat_model( model = self.model_name , model_provider = 'google_genai' ,\
+                google_api_key = 'AIzaSyDKUGAMTjpKpNxmVGU7Wi3pMM1QTumsYNI'
+                )
 
-def get_similar_content( states : dict ):
-    similar_content = vectorstore.similarity_search(query = states['question'] )
-    states['context'] = similar_content
-    return states 
+        embedding = GoogleGenerativeAIEmbeddings( model = 'models/gemini-embedding-001', \
+                google_api_key = 'AIzaSyDKUGAMTjpKpNxmVGU7Wi3pMM1QTumsYNI'
+                )
 
-def generate_content( prompt,states : dict ):
-    context = '\n\n'.join( document.page_content for document in states['context'] )
-    prompt = prompt.invoke(
-        {
-            'context': context,
-            'question': states['question']
-            }
-        ).to_string() 
-    result = llm.invoke(prompt) 
-    return {'Answer' : result} 
+        vectorstore = InMemoryVectorStore( embedding )
+        vectorstore.add_documents(splited_docs)
+        memory = ConversationSummaryMemory(
+                memory_key = 'chat_history',
+                max_num_tokens = 200,
+                llm = llm,
+                return_messages = True 
+                )
 
-states = get_similar_content( states = {'question' : 'what is llm ?'} )
-generated_content = generate_content( prompt, states )
+        prompt = ChatPromptTemplate.from_template(
+                '''
+                Previous_Conversation : {chat_history}
+                Context : {context}
+                Question : {question}
+                Answer : 
+            '''
+            ) 
 
-print(f'Answer : {generated_content["Answer"].content}')
+        def get_similar_content( states : dict ):
+            similar_content = vectorstore.similarity_search(query = states['question'] )
+            states['context'] = similar_content
+            return states 
 
+        def generate_content( prompt,memory,states : dict ):
+            context = '\n\n'.join( document.page_content for document in states['context'] )
+            raw_prompt = {
+                    'chat_history' : memory.load_memory_variables({})['chat_history'],
+                    'context': context,
+                    'question': states['question']
+                    }
+
+            prompt = prompt.invoke(raw_prompt)
+            result = llm.invoke(prompt)
+            memory.chat_memory.add_user_message(states['question'])
+            memory.chat_memory.add_ai_message(result)
+
+            return {'Answer' : result}
+    
+    def get_response(self,query):
+        
+        states = get_similar_content( states = {'question' : query} )
+        generated_content = generate_content( prompt, states )
+
+        print(f'Answer : {generated_content["Answer"].content}')
